7d05f81976a2474b97278dc3b66736b20abfdb49
dtk1 <2018daulet@mail.ru>
2025-04-12 01:15:09 +0200

    port correct

--- src/server.js
+++ src/server.js
@@ -16,5 +16,5 @@ app.use('/flashcards', flashcardsRoutes);
 app.use('/quiz', quizRoutes);
 app.use('/', healthRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
